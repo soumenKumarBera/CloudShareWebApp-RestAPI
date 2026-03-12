fdb307c1e148cf85d666ff2ca7ff5997202458cd
soumen kumar bera <soumenbera4017@gmail.com>
2026-03-12 20:37:10 +0530

    commit

---
+++ src/main/java/in/kb/main/CloudShareWebAppApplication.java
@@ -0,0 +1,13 @@
+package in.kb.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudShareWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudShareWebAppApplication.class, args);
+	}
+
+}
