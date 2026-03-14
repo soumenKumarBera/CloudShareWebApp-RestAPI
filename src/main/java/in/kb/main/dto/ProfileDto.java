e1a7563852959bc6017f174255f6fbee6c7f62b3
soumen kumar bera <soumenbera4017@gmail.com>
2026-03-14 22:31:28 +0530

    lomvok depandency active

---
+++ src/main/java/in/kb/main/dto/ProfileDto.java
@@ -0,0 +1,27 @@
+package in.kb.main.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProfileDto {
+
+    private Integer id;
+    private String clerkId;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private Integer credits;
+    private String photoUrl;
+    private Instant createdAt;
+
+}
